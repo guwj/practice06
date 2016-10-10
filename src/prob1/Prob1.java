1ce3523541fdeb869871b26ed3b0810200ccdf6f
guwj <guwj@naver.com>
2016-10-10 17:31:10 +0900

    Day1010

--- src/prob1/Prob1.java
+++ src/prob1/Prob1.java
@@ -1,12 +1,14 @@
 package prob1;
 
+import java.io.FileReader;
+
 public class Prob1 {
 	public static void main(String[] args) {
 		String strData1 = "200";
 		String strData2 = null;
 		int intData1 = 0;
 		int intData2 = 150;
-				
+
 		/* 코드를 완성합니다. */
 		intData1 = Integer.parseInt(strData1);
 		strData2 = String.valueOf(intData2);
